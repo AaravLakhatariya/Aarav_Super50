004368c60412398bad9bf9cbe9a2489931d423b5
Phantom <aaravlakhatariya@gmail.com>
2026-09-11 21:15:57 +0530

    Add initial C/C++ build tasks and example programs

---
+++ Samin Sir C/stringpalindrome.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    char str[100],strrev[100];
+    int i,len=0;
+    printf("Enter a name: ");
+    gets(str);
+    for(i=0;str[i]!='\0';i++){
+        len++
+    }
+    for(i=len-1;i!='\0';i--){
+        
+    }
+
+
+
+
+    return 0;
+}
