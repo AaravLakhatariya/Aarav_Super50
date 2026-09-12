947f258fa69a20847883b4fda64363c578718a0b
Phantom <aaravlakhatariya@gmail.com>
2026-09-12 16:13:19 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Samin Sir C/StringReverse.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+
+void main()
+{
+    int i,len=0,j=0;
+    char name[100],revname[100];
+
+    printf("\n enter name ::");
+    gets(name);
+
+    // for(i=0;name[i]!='\0';i++)    {
+    //     printf("\n char = %c",name[i]);
+    // }
+
+    for(i=0;name[i]!='\0';i++){
+        len++;
+    }
+
+    printf("\n len = %d",len);
+  
+    for(i=len-1;i>=0;i--){
+
+    
+        printf("\n The reverse = %c",name[i]);
+
+        revname[j] = name[i];
+        j++;
+    }
+    revname[j]='\0';
+
+    printf("\n revname = %s",revname);
+   
+}
