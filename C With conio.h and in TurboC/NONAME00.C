34fea2f244e32b684249a382ff67558a5952ee18
Phantom <aaravlakhatariya@gmail.com>
2026-09-17 19:41:45 +0530

    Refactor string comparison program to concatenate multiple strings and improve user input handling

--- C With conio.h and in TurboC/NONAME00.C
+++ C With conio.h and in TurboC/NONAME00.C
@@ -1,22 +1,38 @@
-#include<conio.h>
 #include<stdio.h>
-void main(){
-	char name1[50]="	ndia",name2[50]="india";
-	int i=0,same=1;
-	clrscr();
-	while(name1[i]!='\0' && name2[i]!='\0'){
-		if(name1[i]==name2[i]){
-			same=0;
-			break;
-		}//end of if
-		i++;
-	}//end of while
-	if(same==1){
-		printf("\n both string are same !!!!!" );
-	} else{
-		printf("\n Both string are diff.....");
-	}
+#include<conio.h>
+void main() {
+char n1[50],n2[50],n3[50],n4[50];
+int i,j,len1,len2,len3,len4,a,b;
+clrscr();
+	printf("Enter String 1: ");
+	gets(n1);
+	printf("\nEnter String 2: ");
+	gets(n2);
+	printf("\nEnter String 3: ");
+	gets(n3);
+	printf("\nEnter String 4: ");
+	gets(n4);
+		for(i=0;n1[i]<='\0';i++){
+			len1++;
+		}
+			for(i=0;n2[i]<='\0';i++){
+			len2++;
+		}
+			for(i=0;n3[i]<='\0';i++){
+			len3++;
+		}
+			for(i=0;n4[i]<='\0';i++){
+			len4++;
+		}
+		a=len1+len2+len3+len4;
+		for(j=0;j<=n1[i];j++){
+		for(i=len1;n1[i]<=a;i++){
+			n1[i]=n2[j],n3[j],n4[j];
+		}
+		}
+		n1[i]='\0';
+		printf("\n The concated string is %s",n1);
 
 
-	getch();
-}//end of main
+getch();
+}
